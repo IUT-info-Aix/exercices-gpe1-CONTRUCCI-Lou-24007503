f03ef5df764ab5abef2b43d80ab03cb6788ebaed
Lou <lou.contrucci@etu.univ-amu.fr>
2025-05-06 15:06:54 +0200

    Exercice 8 fonctionnelle

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,30 @@
 package fr.amu.iut.exercice8;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+
 
 public class LoginControl extends GridPane {
 
-    private void okClicked() {
-    }
+    @FXML
+    private TextField user;
+    @FXML
+    private PasswordField pwd;
 
+    @FXML
+    private void okClicked() {
+        System.out.println( "user Id : " + user.getText());
+
+        String star = "*".repeat(pwd.getLength());
+        System.out.println("Password : " + star);
+
+
+    }
+@FXML
     private void cancelClicked() {
+        user.clear();
+        pwd.clear();
     }
 }
